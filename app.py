81bb4f92e7d65b44ff237015cb587fbdf496e855
beyrouti <michael@beyrouti.co>
2021-10-04 10:23:02 -0400

    modified app to obtain env key and added config

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
+import os
 from flask import Flask
+# from flask_restful import Api, Resource
 
 app = Flask(__name__)
+env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
+# api = Api(app)
+
+# class HelloWorld(Resource):
+#     def get(self):
+#         return {"data": "Hello World"}
+
+#     def post(self):
+#         return {"data": "Posted"}
+
+# api.add_resource(HelloWorld, "/helloworld")
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
 
 @app.route("/")
 def index():
-    return "Hello World! Season 3"
+    secret_key = app.config.get("SECRET_KEY")
+    return f"The configured secret key is {secret_key}."
